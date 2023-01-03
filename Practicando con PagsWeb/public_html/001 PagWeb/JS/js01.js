80712a66bc8bed632e354d6a09bb7bd98d0567c5
adalsus <adalbertomonar@hotmail.com>
2023-01-03 18:41:16 -0500

    Update: Emulando a un menú.

--- Practicando con PagsWeb/public_html/001 PagWeb/JS/js01.js
+++ Practicando con PagsWeb/public_html/001 PagWeb/JS/js01.js
@@ -32,17 +32,30 @@ function mi_bmDV01_click() {
     inJSidX_bmDV01_.setAttribute('style', 'display: block; margin-left: auto;');
     if (window.innerWidth>=640) {
         inJSidmenumain.setAttribute('style', 'display: flex; justify-content: space-around; align-items: center; \n\
-                                                                height: 3em; background: #000000; color: #f0ff00;');
-        for (let e=0; e<inJSclassopmmTodos.length; e++) {
-            inJSclassopmmTodos[e].setAttribute('style', 'display: flex; color: #f0ff00; background: #000000;');
+                                                                background: #000000; color: #f0ff00; padding: 0.125em 0;');
+        for (let elem of inJSclassopmmTodos) {
+            elem.setAttribute('style', 'display: flex; color: #f0ff00; background: #000000;');
         }
     } else {
         inJSidmenumain.setAttribute('style', 'display: grid; place-items: center; height: auto; \n\
                                                                 background: #000000; color: #f0ff00;');
-        for (let e=0; e<inJSclassopmmTodos.length; e++) {
-            inJSclassopmmTodos[e].setAttribute('style', 'padding: 0.5em 0px; background: #000000;');
+        for (let elem of inJSclassopmmTodos) {
+            elem.setAttribute('style', 'background: #000000;');
         }
     }
 }
 
+function mouseOver(e) {
+    const inJSclassopmmTodos = document.querySelectorAll('.opmm');
+    inJSclassopmmTodos[e].style.backgroundColor = 'red';
+}
 
+function mouseOut(e) {
+    const inJSclassopmmTodos = document.querySelectorAll('.opmm');
+    inJSclassopmmTodos[e].style.backgroundColor = 'black';
+}
+
+function opmmCursos_click() {
+    escuchaWindowSize();
+    location.href='pagweb_001.html#secCursos';
+}
