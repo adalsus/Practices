569529c8eeb471eb0225710eabe8dd2314ae9b35
adalsus <adalbertomonar@hotmail.com>
2023-01-29 01:41:31 -0500

    JS: Implementé anclarAvan y quité un id en el script 009 ord insertion.js

--- Practicando con JS/javascript/009 ord insertion.js
+++ Practicando con JS/javascript/009 ord insertion.js
@@ -1,31 +1,34 @@
 function insertionSort(array) {
     //Hecho por adalsus
-    let iscomoMenor;
     let verposiDelmin;
     let desde=0,avan=0;
-    while (avan<=array.length-2) {
+    while (desde<=array.length-2) {
+        let anclarAvan = undefined;
         for (avan=desde; avan<=array.length-2; avan++) {
             if (array[avan]!==array[avan+1]) {
                 
                 if (array[avan+1]<array[avan]) {
                     [array[avan],array[avan+1]] = [array[avan+1],array[avan]];
-                    iscomoMenor=array[avan]; verposiDelmin = avan;
+                    
+                    if (anclarAvan===undefined) {anclarAvan=avan+1}
+                    verposiDelmin = avan;
                     
                     if (verposiDelmin===0) {
-                        desde = avan+1; break;
-                    } else { 
+                        desde = anclarAvan; break;
+                    } else {
                         desde = avan-1; avan -= 2;
                     }
                 }
 
             }
         }
+        if (avan===array.length-1) {desde=array.length-1}
     }
     return array;
 }
 
 
-let lista = [5,8,2,1,0,-8,2,10,5];
+let lista = [5,8,1,0,-8,2,1,9,3,7,-5];
 console.log(`Lista -> ${lista}`)
 insertionSort(lista);
 console.log(`Con insertionSort -> ${lista}`);
